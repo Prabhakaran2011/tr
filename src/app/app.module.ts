524a36c0048ab39bb9653fbe71f33da32728ecab
Prabhakaran <pk.elrevelador@gmail.com>
2017-08-09 10:16:32 +0530

    Haedear and Footer Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { QueryUsComponent } from './components/query-us/query-us.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 // routing
 import { AppRoutingModule } from './shared/app-routing/app-routing.module';
@@ -13,19 +16,27 @@ import { RouterLinkActive, RouterModule } from '@angular/router';
 // services
 import { HttpModule } from '@angular/http';
 
+// angular material
+import { MaterialModule } from '@angular/material';
+
+// third party plugins
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     PagenotfoundComponent,
-    QueryUsComponent
+    QueryUsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     HttpModule,
-    RouterModule
+    RouterModule,    
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'footer-component',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {}
